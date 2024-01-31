35287c7ce3ad3e6ba63ea0d04d79eab5ecb9db31
CDCAnsh <ansh@curiousdevelopers.community>
2024-01-31 15:02:20 +0530

    changed the desing of the page

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -22,16 +22,14 @@ const Card = ({selectedCategory}) => {
 
   return (
     <>
-    <div className='flex md:gap-8 justify-around gap-2 my-4 md:mt-4 md:mb-0 p-4 '>
-      <div onClick={handleUpcomingEvent} className={activeEvent === 'upcoming' ? 'text-xl transition-all duration-400 border-b-4 border-customColor cursor-pointer hover:text-customColor' : 'text-xl transition-all duration-400 border-b-4 border-side cursor-pointer hover:text-customColor'}>Upcoming Events</div>
+    <div className='flex md:gap-8   rounded w-[400px] justify-around gap-2 ml-14 my-4 md:mt-4 md:mb-0 p-4 '>
+      <div onClick={handleUpcomingEvent} className={activeEvent === 'upcoming' ? 'text-xl transition-all duration-400  cursor-pointer hover:text-[#f39f2ad2]' : 'text-xl transition-all duration-400   cursor-pointer hover:text-[#f39f2ad2]'}>Upcoming Events</div>
 
-      <div onClick={handlePrerecordedEvent} className={activeEvent === 'pre-recorded' ? 'text-xl transition-all duration-400 border-b-4 border-customColor cursor-pointer hover:text-customColor' : 'text-xl transition-all duration-400 border-b-4 border-side cursor-pointer hover:text-customColor'}>Pre-recorded Events</div>
+      <div onClick={handlePrerecordedEvent} className={activeEvent === 'pre-recorded' ? 'text-xl transition-all duration-400   cursor-pointer hover:text-[#f39f2ad2]' : 'text-xl transition-all duration-400   cursor-pointer hover:text-[#f39f2ad2]'}>Pre-recorded Events</div>
 
-      <div onClick={handle} className={activeEvent === 'request' ? 'text-xl transition-all duration-400 border-b-4 border-customColor cursor-pointer hover:text-customColor' : 'text-xl transition-all duration-400 border-b-4 border-side cursor-pointer hover:text-customColor'}><a>Request Event</a></div> 
-
-      {/*After applying browser router will use Link tag*/}
+      {/* <div onClick={handle} className={activeEvent === 'request' ? 'text-xl transition-all duration-400 border-b-4 border-customColor cursor-pointer hover:text-customColor' : 'text-xl transition-all duration-400 border-b-4 border-side cursor-pointer hover:text-customColor'}><a>Request Event</a></div> 
       
-      <div  onClick={handle} className={activeEvent === 'schedule' ? 'text-xl transition-all duration-400 border-b-4 border-customColor cursor-pointer hover:text-customColor' : 'text-xl transition-all duration-400 border-b-4 border-side cursor-pointer hover:text-customColor'}><a >Schedule Event</a></div>
+      <div  onClick={handle} className={activeEvent === 'schedule' ? 'text-xl transition-all duration-400 border-b-4 border-customColor cursor-pointer hover:text-customColor' : 'text-xl transition-all duration-400 border-b-4 border-side cursor-pointer hover:text-customColor'}><a >Schedule Event</a></div> */}
     </div>
     <div className="flex justify-start ml-10 flex-wrap">
     {events
