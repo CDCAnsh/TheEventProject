ccd3abd2e6ca4599b120d66a64819251ff266214
Ankit Kumar <ak3311g@gmail.com>
2024-02-09 22:14:48 +0530

    sidenav

--- vite-project/src/components/MainPage.jsx
+++ vite-project/src/components/MainPage.jsx
@@ -1,5 +1,4 @@
 import {React,useState} from 'react'
-import Navbar from './Navbar'
 import CustomContainer from './CustomContainer'
 import SideNav from './SideNav'
 import Card from './Card'
@@ -18,7 +17,6 @@ const MainPage = () => {
         <SideNav onCategoryChange={handleCategoryChange}/>
       </div>      
       <div className='flex flex-col flex-1 z-10 pl-10 md:ml-0'>
-        <Navbar />
         <CustomContainer />
         <Card selectedCategory={selectedCategory}/>
       </div>
